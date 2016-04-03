b515d0ef144ce899affc5c1ee585d9875ab7e220
hukezhu <595632239@163.com>
2016-04-03 23:11:47 +0800

    上传项目

--- index.ios.js
+++ index.ios.js
@@ -393,8 +393,8 @@ AppRegistry.registerComponent("app",() => app);
 
 
 
-
-
+/*
+简单图片浏览器
 
 var React = require('react-native');
 var {
@@ -505,8 +505,181 @@ var styles = StyleSheet.create({
 
 AppRegistry.registerComponent('app', ()=> app);
 
+*/
 
 
+
+
+
+/*webView的使用*/
+/*
+var React = require('react-native');
+var Dimensions = require('Dimensions');
+
+var {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	WebView,
+	View
+} = React;
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
+
+var app = React.createClass({
+	render: function(){
+		return (
+			<View style={styles.container}>
+				<WebView injectedJavaScript="alert('欢迎使用React Native')"
+						 contentInset={{left:-10,top:-28}}
+						 scrollEnabled={false}
+						 html='<div><img src="http://vczero.github.io/ctrip/star_page.jpg"></div>'
+						 bounces = {false}>
+						 url='http://weibo.com/vczero'
+						 style={{width: width,height: height}}
+				</WebView>
+			</View>
+			);
+	}
+});
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1
+	}
+});
+
+AppRegistry.registerComponent('app', ()=> app);
+
+
+*/
+
+
+
+
+/*TabBar的切换*/
+
+
+/*
+
+var React = require('react-native');
+var Dimensions = require('Dimensions');
+
+var {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Image,
+	ScrollView,
+	TabBarIOS
+} = React;
+
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height - 70;
+var app = React.createClass({
+	getInitialState: function(){
+		return {
+			tab: 'message'
+		};
+	},
+	select: function(tabName){
+		this.setState({
+			tab: tabName
+		});
+	},
+	render: function(){
+		return (
+			<TabBarIOS style={styles.flex}>
+				<TabBarIOS.Item title="消息"
+								icon={require("image!message")}
+								onPress={this.select.bind(this,'message')}
+								selected={this.state.tab === 'message'}>
+					<ScrollView>
+						<View style={styles.message}>
+							<Text style={styles.message_title}>南山南</Text>
+							<Text>
+								他不在和谁谈论相逢的孤岛,因为心里早已荒芜人烟
+								他的心里再装不下一个家,做一个只对自己说谎的哑巴,他说
+								呵呵哒
+								他不在和谁谈论相逢的孤岛,因为心里早已荒芜人烟
+								他的心里再装不下一个家,做一个只对自己说谎的哑巴,他说
+								呵呵哒								
+								他不在和谁谈论相逢的孤岛,因为心里早已荒芜人烟
+								他的心里再装不下一个家,做一个只对自己说谎的哑巴,他说
+								呵呵哒								
+								他不在和谁谈论相逢的孤岛,因为心里早已荒芜人烟
+								他的心里再装不下一个家,做一个只对自己说谎的哑巴,他说
+								呵呵哒								
+								他不在和谁谈论相逢的孤岛,因为心里早已荒芜人烟
+								他的心里再装不下一个家,做一个只对自己说谎的哑巴,他说
+								呵呵哒
+							</Text>
+						</View>
+					</ScrollView>
+				</TabBarIOS.Item>
+				<TabBarIOS.Item title="联系人"
+								icon={require("image!phone")}
+								onPress={this.select.bind(this,'phonelist')}
+								selected={this.state.tab === 'phonelist'}>
+					<ScrollView>
+						<Text style={styles.list}>
+							<Text>唐三藏</Text>
+							<Text>13189049076</Text>
+						</Text>
+						<Text style={styles.list}>
+							<Text>孙悟空</Text>
+							<Text>13189049077</Text>
+						</Text>
+						<Text style={styles.list}>
+							<Text>猪八戒</Text>
+							<Text>13189049078</Text>
+						</Text>
+						<Text style={styles.list}>
+							<Text>沙和尚</Text>
+							<Text>13189049079</Text>
+						</Text>
+					</ScrollView>
+				</TabBarIOS.Item>
+				<TabBarIOS.Item title="动态"
+								icon={require("image!star")}
+								onPress={this.select.bind(this,'star')}>
+					<ScrollView style={styles.flex}>
+						<Image style={{width: width,height: height}} source={{uri:'http://vczero.github.io/ctrip/star_page.jpg'}}/>
+					</ScrollView>
+				</TabBarIOS.Item>
+			</TabBarIOS>
+			);
+	}
+});
+
+var styles = StyleSheet.create({
+	flex: {
+		flex : 1
+	},
+	message: {
+		alignItems: 'center',
+		marginRight: 5,
+		marginLeft: 5
+	},
+	message_title:{
+		fontSize: 18,
+		color: '#18B5FF',
+		marginBottom: 5
+	},
+	list: {
+		height: 30,
+		fontSize: 15,
+		marginLeft: 10,
+		marginTop: 10
+	},
+});
+
+AppRegistry.registerComponent('app',()=> app);
+
+
+*/
 
 
 
