23714ab7dac0f2c513380db735da119b3204a45b
IT05 <IT05@21golf.com>
2016-04-19 14:23:30 +0800

    第五章

--- index.ios.js
+++ index.ios.js
@@ -780,6 +780,7 @@ AppRegistry.registerComponent('app', ()=> app);
 
 //4.2  案例:购物车
 
+/*
 var React = require('react-native');
 var {
 	AppRegistry,
@@ -1101,6 +1102,114 @@ var styles = StyleSheet.create({
 });
 
 AppRegistry.registerComponent('app', ()=> app);
+
+
+*/
+
+
+
+
+
+
+//第五章
+
+
+'use strict';
+
+var React = require('react-native');
+
+var {
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View
+} = React;
+
+
+var DeviceManager = require('react-native-device-extension');
+
+console.log(require('NativeModules'));
+
+var PieChart = require('react-native-xypiechart');
+
+var handler = function(body) {
+
+	console.log(body.NativeEvent);
+}
+
+
+var chartData = [{
+	'label' : 'Chrome',
+	value : 36,
+	color : '#008800'
+},{
+	'label' : 'IE8.0',
+	value : 22 ,
+	color : '#0044bb'
+},{
+	'label' : 'Other',
+	value : 42 ,
+	color : '#444444'
+}]
+
+var app = React.createClass({
+	getInitialState : function(){
+		return {orientation: 'unknown'};
+	},
+
+	viewDidOrientation: function(event) {
+		this.state.orientation = event.Orientation;
+	},
+	componentDidMount: function(){
+		DeviceManager.addListener(DeviceManager.events.DEVICE_ORIENTATION_EVENT,this.viewDidOrientation);
+	},
+
+	componentWillUnmount: function() {
+		DeviceManager.removeListener(DeviceManager.events.DEVICE_ORIENTATION_EVENT,this.viewDidOrientation);
+	},
+	render : function(){
+		return (
+			<View style={styles.container}>
+				<PieChart style={styles.chart}>
+					chartData = {chartData}
+					showPercentage = {true}
+					onChange={(handler)}
+				</PieChart>
+			</View>
+			);
+	}
+});
+
+var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	},
+	chart :{
+		width: 200,
+		height: 200
+	}
+});
+
+AppRegistry.registerComponent('app',()=> app);
+
+
+
+
+
+
 
 
 
